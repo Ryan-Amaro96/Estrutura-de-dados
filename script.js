e8b7855596f63d18c42c05f6f95c039a88f8d1d3
Ryan Amaro <ryanamaro2003.st@gmail.com>
2022-06-09 16:26:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+        let name =Ryan;
+        let age =19;
+        let year =2022;
+        let birth = year - age;
+        console.log('birth');
+        let text = Estou mexendo no console.log;
+        console.log('text');
+        let quartaFeira = true;
+        console.log('quartaFeira');
